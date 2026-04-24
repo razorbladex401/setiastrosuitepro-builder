353225b1f57446db354873c5fc23aaa24a30c03a
razorbladex401 <razorbladex401@gmail.com>
2026-04-24 09:13:15 -0400

    Fixes

--- scripts/build_flatpak.sh
+++ scripts/build_flatpak.sh
@@ -110,6 +110,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak-builder \
   --force-clean \
   --install-deps-from=flathub \
+  --allow-network \
   --repo="$REPO_DIR" \
   "$BUILD_DIR" \
   "$MANIFEST_FILE"
